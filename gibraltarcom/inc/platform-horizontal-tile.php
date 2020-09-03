d3123d3a7290f0c45335d5b8484a0b04cc29fc40
Brett Widmann <brett.widmann@gmail.com>
2020-09-03 01:49:17 -0500

    Created new platform snippet without header and title for social page template.

--- gibraltarcom/inc/platform-horizontal-tile.php
+++ gibraltarcom/inc/platform-horizontal-tile.php
@@ -3,7 +3,7 @@ $block_text = get_field('block_text');
 $button = get_field('button');
 if(have_rows('boxes_horizontal') || $button || $block_title || $block_text){ ?>
 <section class="atricles-block bg-light bg-extended horiztonal">
-	<?php if($block_title || $block_text){ ?>
+	<?php if($block_title || $block_text && !is_page('social-responsibility')){ ?>
 	<header class="section-header text-center">
 		<?php if($block_title){ ?><h2><?php echo $block_title; ?></h2><?php } ?>
 		<?php echo $block_text ?>
