fea2036553665c95373104753359fb5763be8544
Brett Widmann <brett.widmann@gmail.com>
2020-09-03 01:59:39 -0500

    Added new block to remove header from social template's image/text fields.

---
+++ gibraltarcom/inc/image-text-combo-social.php
@@ -0,0 +1,25 @@
+<?php
+
+if(have_rows('image_text_combo') || $block_headline || $block_subhead){ ?>
+	<section class="bg-light image-text-combo">
+		<div class="combo">
+			<?php while(have_rows('image_text_combo')){ the_row();
+				$copy = get_sub_field('copy'); ?>
+				<article class="article-post">
+					<?php if($image = get_sub_field('image')){ 
+						$image_1x = $image['image_1x'];
+						$image_2x = $image['image_2x'];
+					?>
+						<picture>
+							<!--[if IE 9]><video style="display: none;"><![endif]-->
+							<source srcset="<?= $image_1x['url'] .', '. $image_1x['url']?> 2x" media="(max-width: 639px)">
+							<!--[if IE 9]></video><![endif]-->
+							<img src="<?=$image_1x['url']?>" alt="<?=$image_1x['alt']?>">
+						</picture>	
+						<?php
+						} ?>
+						<div class="text-holder"><?php echo $copy ?></div>
+				</article>
+			<?php } ?>
+	</section>
+<?php } ?>
